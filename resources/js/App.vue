74bbea8c77e3bddb7ce130ab73e51fcf9875188b
Kostya <koooooostaaa@gmail.com>
2023-02-07 09:56:43 +0200

    added models && migrations

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container mx-auto pt-4">App</div>
+</template>
+
+<script>
+export default {
+    name: "App.vue"
+}
+</script>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <div id="app">
+        <v-app></v-app>
+    </div>
+@endsection
