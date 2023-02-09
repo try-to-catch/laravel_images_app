e3d5d6560f4a2b3f230c7ef6840e28ce013cf817
Kostya <koooooostaaa@gmail.com>
2023-02-09 15:46:25 +0200

    add migration for image table

---
+++ database/migrations/2023_02_09_133917_add_column_thumbnail_url_to_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->string('thumbnail_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->removeColumn('thumbnail_url');
+        });
+    }
+};
